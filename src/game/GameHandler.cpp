d9035e4c3e649f878f4c19e2474df39c8cfe3e3b
Agustin Perez Paladini <agudpp@gmail.com>
2014-10-12 23:07:31 +0200

    Adding some methods to the GameHandler, didn't tested yet

--- src/game/GameHandler.cpp
+++ src/game/GameHandler.cpp
@@ -5,7 +5,9 @@
  *      Author: agustin
  */
 
-#include <game/GameHandler.h>
+#include "GameHandler.h"
+
+#include <common/debug/DebugUtil.h>
 
 namespace game {
 
@@ -18,6 +20,34 @@ GameHandler::GameHandler()
 GameHandler::~GameHandler()
 {
     // TODO Auto-generated destructor stub
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+void
+GameHandler::addEntity(GameEntity* entity)
+{
+    const int index = getEntityIndex(entity);
+    if (index >= 0) {
+        debugWARNING("Trying to add an entity that already exists?\n");
+        return;
+    }
+
+    m_entities.push_back(entity);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void
+GameHandler::removeEntity(GameEntity* entity)
+{
+    const int index = getEntityIndex(entity);
+    if (index < 0) {
+        debugWARNING("Trying to remove an entity that doesn't exists\n");
+        return;
+    }
+    // swap with the last one
+    m_entities[index] = m_entities.back();
+    m_entities.pop_back();
 }
 
 } /* namespace game */
--- src/game/GameHandler.h
+++ src/game/GameHandler.h
@@ -8,15 +8,86 @@
 #ifndef GAMEHANDLER_H_
 #define GAMEHANDLER_H_
 
+
+#include <vector>
+
+#include "GameEntity.h"
+
 namespace game {
 
 class GameHandler
 {
 public:
     GameHandler();
-    virtual
     ~GameHandler();
+
+    // @brief Add a new GameEntity to the list of entities tracked in the game
+    //        This class will NOT handle memory.
+    // @param entity        The entity
+    //
+    void
+    addEntity(GameEntity* entity);
+
+    // @brief Remove an entity if it exists (will not free the memory)
+    // @param entity        The entity to remove
+    //
+    void
+    removeEntity(GameEntity* entity);
+
+    // @brief This is the main method that will update all the entities of
+    //        the game
+    // @param timeFrame     The time frame used to update the entities
+    //
+    inline void
+    update(float timeFrame);
+
+private:
+
+    // @brief Return the associated index for a given entity in the currect
+    //        vector
+    // @return index >= 0 or < 0 if not found
+    //
+    inline int
+    getEntityIndex(const GameEntity* entity) const;
+
+
+private:
+    std::vector<GameEntity*> m_entities;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Inline stuff
+//
+
+inline int
+GameHandler::getEntityIndex(const GameEntity* entity) const
+{
+    for (unsigned int i = 0; i < m_entities.size(); ++i) {
+        if (m_entities[i] == entity) {
+            return static_cast<int>(i);
+        }
+    }
+    return -1;
+}
+
+inline void
+GameHandler::update(float timeFrame)
+{
+    for (GameEntity* ent : m_entities) {
+        ent->update(timeFrame);
+    }
+}
 
 } /* namespace game */
 
